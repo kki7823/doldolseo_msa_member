61659eea9957e2e4b5b71b9b4db1ba863f81c776
kki7823 <kjd55399997@gmail.com>
2021-11-29 16:01:09 +0900

    Initial commit

---
+++ src/main/java/com/gikim/doldolseo_msa_member/DoldolseoMsaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.gikim.doldolseo_msa_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoldolseoMsaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoldolseoMsaMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gikim/doldolseo_msa_member/DoldolseoMsaMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gikim.doldolseo_msa_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoldolseoMsaMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
